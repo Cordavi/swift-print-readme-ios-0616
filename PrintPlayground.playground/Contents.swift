f07dab9c04f3e051d397e58c44ec8176d5091359
Cordavi <michaelamundsen@gmail.com>
2016-07-05 13:08:57 -0400

    Created playground, create variables for favorite color, musician, and word. Printed variables

---
+++ PrintPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let favoriteColor = "Orange"
+let favoriteWord = "Swiftining"
+let favoriteMusician = "Above & Beyond"
+
+print(favoriteColor)
+print(favoriteWord)
+print(favoriteMusician)
+
+print("I'm printing a string in swift")
+print("Hi, Mom! My favorite word is serendipity, my favorite color is green, and my favorite musician is Billy Joel.")
+
+let color = "blue"
+print("The sky is \(color).")
+
+print("Hi, Mom! My favorite word is \(favoriteWord), my favorite color is \(favoriteColor), and my favorite musician is \(favoriteMusician).")
+
+var favoriteThings = ["\(favoriteColor)", "\(favoriteWord)", "\(favoriteMusician)"]
+print(favoriteThings)
